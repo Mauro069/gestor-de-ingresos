f85035e53990f25a98e844656e685faf25671bb9
Mauro Vera <Maurovera069@gmail.com>
2022-09-21 13:04:55 -0300

    Agregue la prop initial money al esquema de reporte

--- api/src/models/ReportSchema.js
+++ api/src/models/ReportSchema.js
@@ -4,6 +4,7 @@ const ReportSchema = new Schema(
   {
     userRef: { type: Schema.ObjectId, ref: "User" },
     month: { type: String, required: true },
+    initialMoney: { type: Number, required: true },
   },
   {
     versionKey: false,
