b1d49b5ca3ba0f1ac42b5b81bd68cb9bba7f2f66
Mauro Vera <Maurovera069@gmail.com>
2022-09-21 13:03:17 -0300

    Agregue metodo para buscar gastos por reporte y por tipo de gasto. Tambien meti validaciones

--- api/src/controllers/expenseController.js
+++ api/src/controllers/expenseController.js
@@ -1,8 +1,19 @@
+const { isValidObjectId } = require("mongoose");
 const Expense = require("../models/ExpenseSchema");
 
 const createExpense = async (req, res) => {
   try {
     const { reportRef, description, date, type } = req.body;
+
+    if (!reportRef || !description || !date || !type) {
+      res.json({ msj: "Te falto enviar algun campo" });
+    }
+
+    if (!isValidObjectId(reportRef) || !isValidObjectId(type)) {
+      res.json({
+        msj: "El report ref o el tipo de gasto no es valido",
+      });
+    }
 
     const newExpense = new Expense({
       reportRef,
@@ -14,26 +25,75 @@ const createExpense = async (req, res) => {
     await newExpense.save();
     res.json({ msj: "Gasto cargado correctamente", expense: newExpense });
   } catch (e) {
-    res.send("Ocurrio un Error", e);
+    res.json({ msj: "Ocurrio un error", error });
   }
 };
 
 const getExpenses = async (req, res) => {
-  const expenses = await Expense.find().populate(["reportRef", "type"]);
-  res.json({ expenses });
+  try {
+    const expenses = await Expense.find().populate(["reportRef", "type"]);
+    res.json({ expenses });
+  } catch (error) {
+    res.json({ msj: "Ocurrio un error", error });
+  }
 };
 
 const getExpensesByReport = async (req, res) => {
-  const { userId } = req.params;
+  const { reportId } = req.params;
 
-  const expenses = await Expense.find({
-    reportRef: userId,
-  }).populate(["reportRef", "type"]);
-  res.json({ expenses });
+  if (!reportId) {
+    res.json({
+      msj: "Debes enviar un reportId",
+    });
+  }
+
+  if (!isValidObjectId(reportId)) {
+    res.json({
+      msj: "El reportId no es valido",
+    });
+  }
+
+  try {
+    const expenses = await Expense.find({
+      reportRef: reportId,
+    }).populate("type");
+
+    res.json({ expenses });
+  } catch (error) {
+    res.json({ msj: "Ocurrio un error", error });
+  }
+};
+
+const getExpensesByExpenseType = async (req, res) => {
+  const { reportId, expenseTypeId } = req.params;
+
+  if (!reportId && !expenseTypeId) {
+    res.json({
+      msj: "Debes enviar un reportId y un expenseTypeId",
+    });
+  }
+
+  if (!isValidObjectId(reportId) || !isValidObjectId(expenseTypeId)) {
+    res.json({
+      msj: "El reportId o el expenseTypeId no es valido",
+    });
+  }
+
+  try {
+    const expenses = await Expense.find({
+      reportRef: reportId,
+      type: expenseTypeId,
+    }).populate(["reportRef", "type"]);
+
+    res.json({ expenses });
+  } catch (error) {
+    res.json({ msj: "Ocurrio un error", error });
+  }
 };
 
 module.exports = {
   createExpense,
   getExpenses,
   getExpensesByReport,
+  getExpensesByExpenseType,
 };
