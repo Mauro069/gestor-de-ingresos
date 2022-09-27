b3e81e819545dd48c64fa839592588fd8ecdc269
Mauro Vera <Maurovera069@gmail.com>
2022-09-27 11:25:00 -0300

    Agregue context y rutas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home, Login, ReportDetail } from "./pages";
+import { Home, Login, Register, ReportDetail } from "./pages";
 import { routes } from "./routes";
+
+import StateProvider from "./context";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.login} element={<Login />} />
-        <Route path={routes.home} element={<Home />} />
-        <Route path={routes.reportDetail} element={<ReportDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <StateProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.register} element={<Register />} />
+          <Route path={routes.login} element={<Login />} />
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.reportDetail} element={<ReportDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </StateProvider>
   );
 }
 
