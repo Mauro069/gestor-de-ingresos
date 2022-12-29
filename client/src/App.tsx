d82166fa87d3b891951dbd1af9ad5a2fbfc88569
Mauro Vera <Maurovera069@gmail.com>
2022-12-29 09:55:11 -0300

    Saque el notification context de aca

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Login, Register, ReportDetail } from "./pages";
 import { routes } from "./routes";
 
-import { AuthProvider } from "./context/AuthContext";
-import { NotificationProvider } from "./context/NotificationContext";
+import StateProvider from "./context";
 
 function App() {
   return (
     <BrowserRouter>
-      <NotificationProvider>
-        <AuthProvider>
-          <Routes>
-            <Route path={routes.register} element={<Register />} />
-            <Route path={routes.login} element={<Login />} />
-            <Route path={routes.home} element={<Home />} />
-            <Route path={routes.reportDetail} element={<ReportDetail />} />
-          </Routes>
-        </AuthProvider>
-      </NotificationProvider>
+      <StateProvider>
+        <Routes>
+          <Route path={routes.register} element={<Register />} />
+          <Route path={routes.login} element={<Login />} />
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.reportDetail} element={<ReportDetail />} />
+        </Routes>
+      </StateProvider>
     </BrowserRouter>
   );
 }
